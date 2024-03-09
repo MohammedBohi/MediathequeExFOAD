1e343d71fdf555ee6aa7a965f1072d0be9ac9c16
MohammedBohi <mohamedbohi2001@gmail.com>
2024-03-10 00:19:46 +0100

    FOAD pattern "Visitor"

---
+++ src/main/java/mediatheque/Item.java
@@ -0,0 +1,26 @@
+package mediatheque;
+
+public abstract class Item {
+	public abstract void accept(Visitor visitor);
+
+	private String title;
+
+	protected Item(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public abstract void print() ;
+
+	@Override
+	public String toString() {
+		return "title=" + title ;
+	}
+	
+}
---
+++ src/main/java/mediatheque/PrintOnlyCDsVisitor.java
@@ -0,0 +1,15 @@
+package mediatheque;
+
+public class PrintOnlyCDsVisitor implements Visitor {
+
+    @Override
+    public void visit(Book book) {
+
+    }
+
+    @Override
+    public void visit(CD cd) {
+        System.out.println("Imprimer les détails du CD : " + cd.getTitle());
+    }
+}
+
